9c851ac86fad92c56c81a2244c13f20c05a6848d
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-06-13 11:38:13 +0200

    error handling

--- index.js
+++ index.js
@@ -10,8 +10,23 @@ var logIncoming = function (req, res, next) {
 app.use(logIncoming);
 
 app.get('/', function (req, res) {
+    throw new Error('sth bad heppened');
     res.send('Hello World!');
 });
+
+app.use(clientError);
+app.use(serverError);
+
+function clientError(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+}
+
+function serverError(err, req, res, next) {
+    console.error(err.stack);
+    res.status(err.status || 500).send('Something broke!');
+}
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
