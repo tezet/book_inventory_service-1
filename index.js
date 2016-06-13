4ac1f555bff786fd6982792af1433ecb77d42c9f
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-06-13 10:57:42 +0200

    using express

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var http = require('http');
-var server = http.createServer(function (req, res) {
-    res.end('hello world');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
 });
-server.listen(3000, function () {
-    console.log("When this callback is invoked our server is listening on port: " + 3000);
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
