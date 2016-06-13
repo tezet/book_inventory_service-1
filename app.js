791173216ba7e58ecfc0ac0621bf1254a6f6c19a
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-06-13 13:44:08 +0200

    upsert

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/bookinventory';
 
 function logRequest(req, res, next) {
     console.log('incoming request logged at ', new Date());
@@ -23,10 +25,18 @@ app.use(bodyParser.json());
 app.use(logRequest);
 
 app.get('/', function (req, res) {
+    MongoClient.connect(url, function (err, db) {
+        console.log("Connected succesfully to server");
+        db.collection('books').updateOne({isbn: req.body.isbn}, {
+            isbn: req.body.isbn,
+            count: req.body.count
+        }, {upsert: true});
+        db.close();
+    });
     res.send('Hello World!');
 });
 
-app.post('/stock', function(req, res) {
+app.post('/stock', function (req, res) {
     res.json({isbn: req.body.isbn, count: req.body.count})
 });
 
