ecb82e46cf095f3fa194d3dc6008944b5f16300e
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-06-13 14:27:41 +0200

    promisification

--- app.js
+++ app.js
@@ -29,28 +29,26 @@ app.get('/', function (req, res) {
 });
 
 var collection;
-MongoClient.connect(url, function (err, db) {
-    if(err) {
-        console.error(err);
-        process.exit(1);
-    }
-    else {
-        console.log("Connected succesfully to server");
-        collection = db.collection('books');
-    }
+var p = MongoClient.connect(url).then(function(db) {
+    return db.collection('books');
 });
 
 
-app.post('/stock', function (req, res) {
-    collection.updateOne({isbn: req.body.isbn}, {
-        isbn: req.body.isbn,
-        count: req.body.count
-    }, {upsert: true});
-    res.json({isbn: req.body.isbn, count: req.body.count});
+app.post('/stock', function (req, res, next) {
+    p.then(function(collection) {
+        return collection.updateOne({isbn: req.body.isbn}, {
+            isbn: req.body.isbn,
+            count: req.body.count
+        }, {upsert: true});
+    }).then(function() {
+        res.json({isbn: req.body.isbn, count: req.body.count});
+    }).catch(next);
 });
 
 app.get('/stock', function (req, res) {
-    collection.find({}).toArray(function (err, results) {
+    p.then(function(collection) {
+        return collection.find({}).toArray();
+    }).then(function(results) {
         res.json(results);
     });
 });
