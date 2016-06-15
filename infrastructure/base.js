6f973a40cc9351482025d2359f80389a2f1f002b
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-06-15 09:18:55 +0200

    adding logdrain

--- infrastructure/base.js
+++ infrastructure/base.js
@@ -17,7 +17,7 @@ var base = {
         'http-sni': {enabled: false}
     },
     formation: [{process: 'web', quantity: 1, size: 'Free'}],
-    log_drains: []
+    log_drains: ['syslog://data.logentries.com:13636']
 };
 
 module.exports = {
